f16fed92f95789699e43b419f97956da403f2402
JetProc <soonjae8297@gmail.com>
2025-10-16 13:22:57 +0900

    feat: implement user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import { INFORMATION_MESSAGE, ERROR_MESSAGE } from './constants.js';
+
 class App {
-	async run() {}
+	async run() {
+		try {
+			const INPUT_STRING = await Console.readLineAsync(INFORMATION_MESSAGE.START);
+		} catch (error) {
+			Console.print(error);
+		}
+	}
 }
 
 export default App;
