b3d8fada07f2156cb76091f3d64dd12a755753ab
JetProc <soonjae8297@gmail.com>
2025-10-20 17:31:10 +0900

    style: add lines

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 import { INFORMATION_MESSAGE } from './constants/index.js';
-import Calculator from './Calculator.js';
+import Calculator from './calculator/Calculator.js';
 
 class App {
   async run() {
     try {
       const input = await Console.readLineAsync(INFORMATION_MESSAGE.START);
+
       const calculator = new Calculator();
-      const result = calculator.add(input);
+
+      const result = calculator.calculate(input);
 
       Console.print(`${INFORMATION_MESSAGE.RESULT}${result}`);
     } catch (error) {
