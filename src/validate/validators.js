65e9e371b7b41c605e26cf59afc8a964eb9c7db9
JetProc <soonjae8297@gmail.com>
2025-10-20 17:35:59 +0900

    refactor: separate the validation logic into one file

---
+++ src/validate/validators.js
@@ -0,0 +1,40 @@
+import { ERROR_MESSAGE, CONSTANT_CHAR } from '../constants/index.js';
+import {
+  isAllNumber,
+  isAllPositiveNumber,
+  isCustomSeparatorExist,
+  isCustomSeparatorChar,
+  hasInputCustomSeparatorStartMarkOnly,
+} from './conditions.js';
+
+// Input 검증
+export function validateEmptyInput(input) {
+  return input.length > 0;
+}
+
+export function validateInputString(input) {
+  if (input.startsWith(CONSTANT_CHAR.CUSTOM_SEPARATOR_START_MARK) && hasInputCustomSeparatorStartMarkOnly(input)) {
+    throw new Error(ERROR_MESSAGE.CUSTOM_SEPARATOR_MUST_BE_CLOSED);
+  }
+}
+
+// Number Type 검증
+export function validateNumberTypes(list) {
+  if (!isAllNumber(list)) {
+    throw new Error(ERROR_MESSAGE.VALUE_MUST_BE_NUMBER);
+  }
+
+  if (!isAllPositiveNumber(list)) {
+    throw new Error(ERROR_MESSAGE.VALUE_MUST_BE_POSITIVE);
+  }
+}
+
+// Separator 검증
+export function validateCustomSeparator(customSeparator) {
+  if (!isCustomSeparatorExist(customSeparator)) {
+    throw new Error(ERROR_MESSAGE.CUSTOM_SEPARATOR_MUST_EXIST);
+  }
+  if (!isCustomSeparatorChar(customSeparator)) {
+    throw new Error(ERROR_MESSAGE.CUSTOM_SEPARATOR_MUST_BE_CHARACTER);
+  }
+}
