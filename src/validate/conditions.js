1a6f515eb18135054650542be8f03bcfdff626a1
JetProc <soonjae8297@gmail.com>
2025-10-20 17:33:43 +0900

    refactor: change validation.js to condition.js

---
+++ src/validate/conditions.js
@@ -0,0 +1,33 @@
+import { CONSTANT_CHAR } from '../constants/index.js';
+import {
+  GENERATE_FIND_CUSTOM_SEPARATOR_START_MARK_ONLY_REGEX,
+  GENERATE_CUSTOM_SEPARATOR_IS_CHAR_REGEX,
+} from '../utils/regexUtils.js';
+
+// Input 관련 조건 확인
+export function hasInputCustomSeparatorStartMarkOnly(input) {
+  const REGEX = GENERATE_FIND_CUSTOM_SEPARATOR_START_MARK_ONLY_REGEX(
+    CONSTANT_CHAR.CUSTOM_SEPARATOR_START_MARK,
+    CONSTANT_CHAR.CUSTOM_SEPARATOR_END_MARK
+  );
+  return REGEX.test(input);
+}
+
+// Number Type 관련 조건 확인
+export function isAllNumber(list) {
+  return list.every((value) => value !== '' && !isNaN(value));
+}
+
+export function isAllPositiveNumber(list) {
+  return list.every((value) => Number(value) > 0);
+}
+
+// Custom Separator 관련 조건 확인
+export function isCustomSeparatorExist(customSeparator) {
+  return customSeparator?.length > 0;
+}
+
+export function isCustomSeparatorChar(customSeparator) {
+  const REGEX = GENERATE_CUSTOM_SEPARATOR_IS_CHAR_REGEX();
+  return REGEX.test(customSeparator);
+}
